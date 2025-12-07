02647528af23b7d460272c8683c3387beb8d7a4c
sumit0224 <sumitgautam969@gmail.com>
2025-12-07 17:31:02 +0530

    feat: initialize backend server with Express, Socket.IO, CORS, and API routes

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,12 @@ const PORT = process.env.PORT || 5000;
 // Init Socket.IO
 initIO(server);
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'https://puncher-app.vercel.app',
+    credentials: true,
+  }
+));
 app.use(express.json());
 
 const authRoutes = require('./src/routes/authRoutes');
